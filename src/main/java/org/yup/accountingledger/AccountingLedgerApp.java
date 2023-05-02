866c5e821c2a3e8291e049302610c4d1304078b6
Santiago Osuna <santiago_osuna@icloud.com>
2023-05-02 10:52:30 -0400

    added screens code

--- src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
+++ src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
@@ -1,9 +1,71 @@
 package org.yup.accountingledger;
-
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
 public class AccountingLedgerApp {
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
 
-        System.out.println("Hello WORLD");
+        boolean appRunning = true;
+
+        while (appRunning) {
+
+            String homeScreenChoice = Screens.homeScreen();
+
+            switch (homeScreenChoice.toUpperCase()) {
+
+                case "D":
+                    LocalDate todaysDate = LocalDate.now();
+
+                    LocalTime currentTime = LocalTime.now();
+
+                    System.out.println("Description: ");
+                    String descriptionInput = userInput.nextLine();
+
+                    System.out.println("Vendor: ");
+                    String vendorInput = userInput.nextLine();
+
+                    System.out.println("Amount: ");
+                    String amountInput = userInput.nextLine();
+
+                    break;
+
+                case "P":
+                    todaysDate = LocalDate.now();
+
+                    currentTime = LocalTime.now();
+
+                    System.out.println("Description: ");
+                    descriptionInput = userInput.nextLine();
+
+                    System.out.println("Vendor: ");
+                    vendorInput = userInput.nextLine();
+
+                    System.out.println("Amount: ");
+                    amountInput = userInput.nextLine();
+
+                    break;
+
+                case "L":
+
+                    break;
+
+                case "X":
+                    appRunning = false;
+                    System.out.println("Exiting app...Goodbye!");
+
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again!\n");
+            }
+
+        }
     }
 }
+
+
+
+
--- src/main/java/org/yup/accountingledger/Screens.java
+++ src/main/java/org/yup/accountingledger/Screens.java
@@ -1,2 +1,22 @@
-package org.yup.accountingledger;public class Screens {
+package org.yup.accountingledger;
+import java.util.Scanner;
+
+public class Screens {
+    public static String homeScreen() {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Welcome to your account ledger!\n");
+
+        System.out.println("What would you like to do today:");
+        System.out.println("-Add a Deposit (Enter D)");
+        System.out.println("-Make a Payment (Enter P)");
+        System.out.println("-Display the Ledger (Enter L)");
+        System.out.println("-Exit App (Enter X)");
+
+        System.out.println("\nEnter your selection here:");
+
+        return userInput.nextLine();
+
+    }
 }
