83e4bed155b17c8d08489e3a7e52384a5aaa2645
Santiago Osuna <santiago_osuna@icloud.com>
2023-05-04 16:02:44 -0400

    finished everything except screen code

--- src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
+++ src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
@@ -9,65 +9,87 @@ import java.util.Scanner;
 public class AccountingLedgerApp {
     public static void main(String[] args) {
 
-        boolean appRuns = true;
+        boolean makesAppRun = true;
 
-        while (appRuns) {
-            String homeScreenChoice = HomeScreen.displayHomeScreen();
+        while (makesAppRun) {
+            String homeScreenChoice = homeScreen();
 
-                //user types a letter in home screen
+                //Allows user to type letter choice lower or upper case
                 switch (homeScreenChoice.toUpperCase()) {
 
+                //User types a letter in the homeScreen method based on the cases below
                 case "D" ->
-                    //prompts deposit
-                    makeTransaction(true);
+                    //prompts deposit in createsTransaction method
+                    createsTransaction(true);
                 case "P" ->
-                    //prompts payment
-                    makeTransaction(false);
+                    //prompts payment in createsTransaction method
+                    createsTransaction(false);
                 case "L" ->
-                    //prompts ledger
+                    //Ledger.java class and ledgerScreen method is called which prompts a new screen with options
                     Ledger.ledgerScreen();
                 case "X" -> {
-                    //prompts exit
-                    appRuns = false;
-                    System.out.println("Exiting app...Goodbye!");
+                    //prompts exit and closes the app
+                    makesAppRun = false;
+                    System.out.println("\n◢◤◢◤◢◤◢◤◢◤◢◤◢◤ Exiting App. Goodbye! ◢◤◢◤◢◤◢◤◢◤◢◤◢◤\n");
                 }
-                default -> System.out.println("Error: Invalid choice, please try again.\n");
+                default -> System.out.println("\n⚠ Error: Invalid choice, please try again.\n");
             }
         }
     }
-    public static void makeTransaction(boolean isDebit) {
+
+    public static String homeScreen() {
+
+        Scanner userInput = new Scanner(System.in);
+
+        //Main display when we run the app
+        System.out.println("Hello Santiago, welcome to your account.");
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("                ◩ HOME SCREEN ◪                 ");
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("Pleas make a selection:");
+        System.out.println("- Add a Deposit (Enter D)");
+        System.out.println("- Add a Payment (Enter P)");
+        System.out.println("- Display the Ledger (Enter L)");
+        System.out.println("- Exit App (Enter X)");
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("Enter your selection here:");
+
+        return userInput.nextLine();
+    }
+
+    public static void createsTransaction(boolean isDebit) {
 
         Scanner userScanner = new Scanner(System.in);
 
+        //Automatically saves current date and time, and formats it in the file
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-
         LocalTime time = LocalTime.now();
         String formattedTime = time.format(formatter);
 
-        System.out.println("Description: ");
+        //Prompts user with the input needed, and saves it
+        System.out.println("Description of your transaction: ");
         String description = userScanner.nextLine();
-
-        System.out.println("Vendor: ");
+        System.out.println("Vendor of your transaction: ");
         String vendor = userScanner.nextLine();
-
-        System.out.println("Amount: ");
+        System.out.println("Amount for your transaction: ");
         double amount = userScanner.nextDouble();
 
+        //Uses saved input to write it to the file
             try {
             FileWriter writer = new FileWriter("transactions.csv", true);
             if (isDebit) {
-                //writes input for deposit to file
+                //writes and formats saved deposit to file
                 writer.append(String.format("%s | %s | %s | %s | %.2f\n", today, formattedTime, description, vendor, amount));
-                System.out.println("Deposit added to file successfully.\n");
+                System.out.println("\n✓ Deposit added to file successfully.\n");
             } else {
-                //writes input for payment to file
+                //writes and formats saved payment to file
                 writer.append(String.format("%s | %s | %s | %s | %.2f\n", today, formattedTime, description, vendor, amount * -1));
-                System.out.println("Payment added to file successfully.\n");
+                System.out.println("\n✓ Payment added to file successfully.\n");
             }
                 writer.close();
             } catch (IOException e) {
-                System.out.println("Error: Could not process data to file.");
+                System.out.println("Error: Could not add data to file.");
         }
     }
 }
--- src/main/java/org/yup/accountingledger/Ledger.java
+++ src/main/java/org/yup/accountingledger/Ledger.java
@@ -6,150 +6,163 @@ import java.util.Scanner;
 
 public class Ledger {
     public static void ledgerScreen() {
-        boolean ledgerRuns = true;
+        boolean makesLedgerRun = true;
 
-        while (ledgerRuns) {
+        //The ledgerScreen method runs a loop that displays options to user
+        while (makesLedgerRun) {
 
             Scanner userScanner = new Scanner(System.in);
 
-            System.out.println("Now displaying Ledger Screen:");
-            System.out.println("-------------------------------------------");
+            System.out.println("════════════════════════════════════════════════");
+            System.out.println("               ◩ LEDGER SCREEN ◪                ");
+            System.out.println("════════════════════════════════════════════════");
             System.out.println("Pleas make a selection:");
             System.out.println("- View all Entries (Enter A)");
             System.out.println("- View Deposits (Enter D)");
             System.out.println("- View Payments (Enter P)");
-            System.out.println("- View Reports (Enter R)");
+            System.out.println("- View Reports Screen (Enter R)");
             System.out.println("- Return to Home Screen (Enter H)");
-            System.out.println("-------------------------------------------");
+            System.out.println("════════════════════════════════════════════════");
             System.out.println("Enter your selection here:");
 
             String userInput = userScanner.nextLine();
 
+            //Reads the user's input and performs an action based on the selection
             switch (userInput.toUpperCase()) {
 
                 case "A" -> {
-
                     try {
+                        //The file is read
                         FileReader readTransactionsCsv = new FileReader("transactions.csv");
-
                         BufferedReader transactionsCsvReader = new BufferedReader(readTransactionsCsv);
-
                         transactionsCsvReader.readLine();
-
                         String transactionLine;
 
+                        //Formats and prints all the data in file to the user
                         while ((transactionLine = transactionsCsvReader.readLine()) != null) {
                             System.out.println(transactionLine + "\n");
                         }
-
                         transactionsCsvReader.close();
-
-                    } catch (IOException e) {
+                    }catch (IOException e){
                         System.out.println("Error: Could not read the file." + e.getMessage());
                     }
                 }
 
                 case "D" -> {
-
                     try {
+                        //The file is read
                         FileReader readTransactionsCsv = new FileReader("transactions.csv");
-
                         BufferedReader transactionsCsvReader = new BufferedReader(readTransactionsCsv);
-
                         transactionsCsvReader.readLine();
-
                         String transactionLine;
 
+                        //Formats and prints only the deposit data we want from the file
                         while ((transactionLine = transactionsCsvReader.readLine()) != null) {
-
                             String[] transactionDepositData = transactionLine.split("\\|");
                             transactionDepositData[4] = transactionDepositData[4].replace("$", "");
                             float transactionAmount = Float.parseFloat(transactionDepositData[4]);
-
                             if (transactionAmount > 0) {
                                 System.out.println(transactionLine + "\n");
                             }
                         }
                         transactionsCsvReader.close();
-                    } catch (IOException e) {
+                    }catch (IOException e){
                         System.out.println("Error: Could not read the file." + e.getMessage());
                     }
                 }
 
                 case "P" -> {
                     try {
+                        //The file is read
                         FileReader readTransactionsCsv = new FileReader("transactions.csv");
-
                         BufferedReader transactionsCsvReader = new BufferedReader(readTransactionsCsv);
-
                         transactionsCsvReader.readLine();
-
                         String transactionLine;
 
+                        //Formats and prints only the payment data we want from the file
                         while ((transactionLine = transactionsCsvReader.readLine()) != null) {
-
                             String[] transactionPaymentData = transactionLine.split("\\|");
                             transactionPaymentData[4] = transactionPaymentData[4].replace("$", "");
                             float transactionAmount = Float.parseFloat(transactionPaymentData[4]);
-
                             if (transactionAmount < 0) {
                                 System.out.println(transactionLine + "\n");
                             }
                         }
                         transactionsCsvReader.close();
-                    } catch (IOException e) {
+                    }catch (IOException e) {
                         System.out.println("Error: Could not read the file." + e.getMessage());
                     }
                 }
 
+                //reportsScreen method is called which displays the reports screen options
                 case "R" -> reportsScreen();
 
                 case "H" -> {
 
-                    System.out.println("Now returning to home screen...");
+                    System.out.println("\n◢◤◢◤◢◤◢◤◢◤◢◤◢◤ Returning to Home screen ◢◤◢◤◢◤◢◤◢◤◢◤◢◤\n");
 
-                    ledgerRuns = false;
+                    makesLedgerRun = false;
                 }
-
-                default -> System.out.println("Error: Invalid choice, please try again.\n");
+                default -> System.out.println("\n⚠ Error: Invalid choice, please try again.\n");
             }
         }
     }
-
     public static void reportsScreen() {
 
-            Scanner userScanner = new Scanner(System.in);
+        Scanner userScanner = new Scanner(System.in);
 
-            System.out.println("Now displaying Reports Screen:");
-            System.out.println("-------------------------------------------");
-            System.out.println("- View Month to Date (Enter 1)");
-            System.out.println("- View Previous Month (Enter 2)");
-            System.out.println("- View Year to Date (Enter 3)");
-            System.out.println("- View Previous Year (Enter 4)");
-            System.out.println("- Search by Vendor (Enter 5)");
-            System.out.println("- Return to Ledger Screen (Enter 0)");
-            System.out.println("-------------------------------------------");
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("              ◩ REPORTS SCREEN ◪                ");
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("- View Month to Date (Enter 1)");
+        System.out.println("- View Previous Month (Enter 2)");
+        System.out.println("- View Year to Date (Enter 3)");
+        System.out.println("- View Previous Year (Enter 4)");
+        System.out.println("- Search by Vendor (Enter 5)");
+        System.out.println("- Return to Ledger Screen (Enter 0)");
+        System.out.println("════════════════════════════════════════════════");
+        System.out.println("Enter your selection here:");
 
-            int reportsUserInput = Integer.parseInt(userScanner.nextLine());
+        int reportsUserInput = Integer.parseInt(userScanner.nextLine());
 
-            switch (reportsUserInput) {
-                case 1 -> System.out.println("test");
+        switch (reportsUserInput) {
+            case 1 -> reportsMonthToDate();
 
-                case 2 -> System.out.println("test2");
+            case 2 -> reportsPreviousMonth();
 
-                case 3 -> System.out.println("test3");
+            case 3 -> reportsYearToDate();
 
-                case 4 -> System.out.println("test4");
+            case 4 -> reportsPreviousYear();
 
-                case 5 -> System.out.println("test5");
+            case 5 -> reportsSearchByVendor();
 
-                case 0 -> System.out.println("Now returning to ledger screen...\n");
+            case 0 -> System.out.println("◢◤◢◤◢◤◢◤◢◤◢◤◢◤ Returning to Ledger Screen ◢◤◢◤◢◤◢◤◢◤◢◤◢◤\n");
 
-                default -> System.out.println("Error: Invalid choice, please try again.\n");
-
-            }
+            default -> System.out.println("\n⚠ Error: Invalid choice, please try again.\n");
         }
     }
+    private static void reportsMonthToDate() {
+
+        System.out.println("Displaying Month to Date Report:");
+    }
+    private static void reportsPreviousMonth() {
+
+    }
+    private static void reportsYearToDate() {
+
+    }
+    private static void reportsPreviousYear() {
+
+    }
+    private static void reportsSearchByVendor() {
+
+        Scanner userScanner = new Scanner(System.in);
+
+        System.out.println("\nEnter vendor name:");
+        String vendorName = userScanner.nextLine();
+
+    }
+}
 
 
 
